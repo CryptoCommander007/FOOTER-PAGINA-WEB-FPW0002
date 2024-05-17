ec0bcfdde149b322aab31b0ab903795fa3b9d53f
CryptoCommander007 <rivantped@gmail.com>
2024-05-17 03:58:41 -0400

    FOOTER-PAGINA-WEB-FPW0002

---
+++ run_commands.bat
@@ -0,0 +1,6 @@
+git init
+git add .
+git commit -m "FOOTER-PAGINA-WEB-FPW0002"
+git branch -M main
+git remote add origin https://github.com/CryptoCommander007/FOOTER-PAGINA-WEB-FPW0002.git
+git push -u origin main
